1769cd391b20f67d08c8177e69d1e06612a1d8a2
anufether <anufether@sina.com>
2023-10-28 19:49:42 +0800

    6.5.1节练习

--- function/function_overload.cc
+++ function/function_overload.cc
@@ -29,10 +29,39 @@ int calc(const int, const int);
 // 6.39.b
 int get();
 // 变量不同，不构成引用
-double get();
+// double get();
 
 int *reset(int *);
 // 返回单位不同
 // double *reset(double *);
 // 构成重载
 int *reset(double *);
+
+std::string read();
+void print(const std::string &);
+// 重载print函数
+void print(double);
+void fooBar(int ival)
+{
+    bool read = false; // 新作用域：隐藏了外层的read
+    // std::string s = read(); // 错误：read是一个布尔值
+}
+
+int ff(int a, int b = 0, int c = 0);
+// int *init(int ht = 24, int wd, char bckgrnd);
+
+std::string make_plural(size_t ctr, const std::string &word = "s",
+                        const std::string &ending = "es")
+{
+    return (ctr > 1) ? word + ending : word;
+}
+
+int main()
+{
+    std::cout << make_plural(2, "success") << std::endl;
+    std::cout << make_plural(2, "failure") << std::endl;
+
+    // 输入三个参数，加个s。。
+
+    return 0;
+}
